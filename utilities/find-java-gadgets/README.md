6f612bc3ea619a4f9537e7b425ef78af848d2c92
highjack <highjack@users.noreply.github.com>
2025-05-16 09:42:55 +0100

    Delete utilities/find-java-gadgets directory

--- utilities/find-java-gadgets/README.md
+++
@@ -1,2 +0,0 @@
-# find-java-gadgets
-Quick script to find usable ysoserial gadgets in a folder
--- utilities/find-java-gadgets/find-gadget.sh
+++
@@ -1,6 +0,0 @@
-for word in $(cat gadgets.txt); 
-do 
-	filename=$(echo $word.jar | sed 's/:/-/';)
-	find $1 -name $filename
-done
-
